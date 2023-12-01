4da5c43a93cb8664dfe3695eb5383a7236a850b7
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-12-01 12:28:27 -0500

    fix linter errors

--- test/models/comment_model_test.rb
+++ test/models/comment_model_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class CommentModelTest < ActiveSupport::TestCase
   # test "the truth" do
--- test/models/user_model_test.rb
+++ test/models/user_model_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UserModelTest < ActiveSupport::TestCase
   # test "the truth" do
