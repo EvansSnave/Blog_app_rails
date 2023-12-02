17eda8260c1bc581d35a3c7422619937d98d3f99
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-12-02 18:23:15 -0500

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,7 @@ class Post < ApplicationRecord
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
