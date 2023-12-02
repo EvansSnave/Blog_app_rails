2de2ab6d42e223602408f0e052a5523f6dd6d234
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-12-02 13:47:12 -0500

    add data successfully

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :user
   belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  def update_comments_counter!
+    post.update(comments_counter: post.comments.count)
+  end
 end
