d2d5b821f4341a0a93749e53e1a53b01d880024f
Ademola Owoeye <ademolaowoeye07@gmail.com>
2023-12-21 19:11:30 +0100

    the post delection

--- app/controllers/post_deletions_controller.rb
+++ app/controllers/post_deletions_controller.rb
@@ -1,7 +1,6 @@
-# post_deletions_controller.rb
+
 class PostDeletionsController < ApplicationController
   before_action :authenticate_user!
-
   def destroy
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id]) # Cambia params[:id] a params[:post_id]
