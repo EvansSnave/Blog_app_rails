c66d0dc38c387c590841c3f62d6b40d0da52110d
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-12-21 14:54:23 -0500

    add ability

--- app/controllers/post_delections_controller.rb
+++ app/controllers/post_delections_controller.rb
@@ -1,5 +1,7 @@
+# post_deletions_controller.rb
 class PostDeletionsController < ApplicationController
   before_action :authenticate_user!
+
   def destroy
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id]) # Cambia params[:id] a params[:post_id]
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   before_action :authenticate_user!
   def index
     @user = User.find(params[:user_id])
